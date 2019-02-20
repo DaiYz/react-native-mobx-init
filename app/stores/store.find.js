9169daafcb91da11cf2ede209fe92d7aced1f59f
DaiYz <dyz2924ever@sina.com>
2019-02-20 14:10:14 +0800

    Update store.find.js
    上拉加载

--- app/stores/store.find.js
+++ app/stores/store.find.js
@@ -18,7 +18,7 @@ class find {
     }
     try {
       const page = more ? this.page + 1 : 1
-      const res = await (await fetch(`https://tcwm.zzsyongfen.top/app/index.php?i=2&t=0&v=1.20&from=wxapp&c=entry&a=wxapp&do=StoreList&m=zh_cjdianc&sign=e763c3663302efd71f6ae7cbc231d7ea&nopsf=2&nostart=2&yhhd=&lat=23.122103&lng=113.367485&page=${page}&pagesize=10`)).json()
+      const res = await (await fetch(`https://XXXXXXX&page=${page}&pagesize=10`)).json()
       this.list = more ? this.list.slice().concat(res) : res
       console.log(res)
       this.page = page
